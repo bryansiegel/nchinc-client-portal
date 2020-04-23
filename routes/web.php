0f3cc8ff63cb818d908bc825c5a57ce2ddc8b805
Bryan Siegel <bryansiegel@gmail.com>
2020-04-23 16:59:47 -0700

    documents index/documents test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('dashboard');
 });
+
+//Documents
+Route::get('documents/', 'DocumentsController@index');
+
+//Forms
+Route::get('forms/', 'FormsController@index');
+
