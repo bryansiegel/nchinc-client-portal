5e0afaf264d124cfa0762263400f9691fd7f78b1
Bryan Siegel <bryansiegel@gmail.com>
2020-04-23 16:40:39 -0700

    document, forms, users, announcement models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company_name');
+            $table->string('address');
+            $table->string('company_address');
+            $table->boolean('active')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('main_phone');
+            $table->string('secondary_phone');
+            $table->string('business_type');
+            //user, developer, admin
+            $table->string('access_level')->default('user');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
