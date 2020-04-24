b4e6a8d83d2b9b559a2ec3f49af3fbb337ed892b
Bryan Siegel <bryansiegel@gmail.com>
2020-04-23 19:55:57 -0700

    documents index design

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -2,4 +2,47 @@
 
 @section('content')
 
+<div class="container justify-content-center">
+    <div><h1>Documents</h1> <span style="float: right;margin-top: -45px; margin-bottom: 25px;"><a href="" class="btn btn-primary">Create A Document</a></span></div>
+
+
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th>Title</th>
+                <th>Document</th>
+                <th>Url</th>
+                <th>Active?</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+            </tr>
+                        <tr>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+            </tr>
+                        <tr>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+            </tr>
+                        <tr>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+                <td>title</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+
 @endsection
